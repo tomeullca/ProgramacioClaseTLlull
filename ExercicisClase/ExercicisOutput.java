42b5b5b0f3bb31c7546848276c027c47de938333
Tomeu <tomeullca@hotmail.com>
2023-11-09 12:10:29 +0100

    ExercicisOutput

---
+++ ExercicisClase/ExercicisOutput.java
@@ -0,0 +1,15 @@
+package ExercicisClase;
+
+public class ExercicisOutput {
+
+
+    public static void main(String[] args){
+        double var01=2.55;
+        System.out.printf("%f", var01);
+    }
+
+
+
+}
+
+
